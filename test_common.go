0ee60e5c3b765f374a14b57242ba30cb14573540
st0n3 <ssst0n3@gmail.com>
2020-06-18 10:04:27 +0800

    update

--- test_common.go
+++ test_common.go
@@ -26,8 +26,9 @@ func (c Connector) ResetAutoIncrementMysql(tableName string) {
 	Logger.Info("set id start from 0")
 	query := fmt.Sprintf("ALTER TABLE %s AUTO_INCREMENT = 1", tableName)
 	Logger.Debugf("query: %s", query)
-	_, err := c.Query(query)
+	_, err := c.Exec(query)
 	if err != nil {
 		Logger.Fatal(err)
 	}
+	//	TODO: need commit or not?
 }
