2082f64bfd1c0d63915fa2c44b6ede8bfa5befef
st0n3 <ssst0n3@gmail.com>
2020-10-09 16:08:21 +0800

    修改：     example/mysql/conn.go 	修改：     go.mod 	修改：     go.sum

--- example/mysql/conn.go
+++ example/mysql/conn.go
@@ -1,6 +1,9 @@
 package mysql
 
-import "github.com/ssst0n3/lightweight_db"
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/ssst0n3/lightweight_db"
+)
 
 func Conn() lightweight_db.Connector {
 	return lightweight_db.GetNewConnector("mysql", lightweight_db.GetDsnFromEnvNormal())
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/davecgh/go-spew v1.1.1
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.6.0
 	github.com/ssst0n3/awesome_libs v0.4.4
