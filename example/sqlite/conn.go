752c5c784cfa862dbdbb99dc591273114f087992
st0n3 <ssst0n3@gmail.com>
2020-10-09 16:05:15 +0800

    修改：     example/sqlite/conn.go

--- example/sqlite/conn.go
+++ example/sqlite/conn.go
@@ -1,6 +1,10 @@
 package sqlite
 
-import "github.com/ssst0n3/lightweight_db"
+import (
+	"github.com/ssst0n3/lightweight_db"
+	_ "modernc.org/sqlite"
+)
+
 
 func Conn() lightweight_db.Connector {
 	return lightweight_db.GetNewConnector("sqlite", lightweight_db.GetDsnFromEnvNormal())
